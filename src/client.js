5ae2bfc588c1fe63b3486d58faa6fa6eeb548c39
Brandon Koeck <bkoeck3@gmail.com>
2026-04-19 22:56:49 -0500

    Moved credentials to env variables

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const URL = 'https://adogcxwmobwunqnatxzx.supabase.co'
-const API_KEY = 'sb_publishable_ahLrOvhbsKRevuU5higmVg_7GV_IzCq'
+const URL = import.meta.env.VITE_SUPABASE_URL
+const API_KEY = import.meta.env.VITE_SUPABASE_API_KEY
 
 export const supabase = createClient(URL, API_KEY)
